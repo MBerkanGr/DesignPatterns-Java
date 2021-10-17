604e1f01a133c0eb3e92002bdcee55a11cd9922f
Mehmet Berkan <mehmetberkangorel@gmail.com>
2021-10-17 23:18:35 +0300

    Add OCP README

---
+++ src/BasicPrinciples/OCP/OCP1.java
@@ -0,0 +1,11 @@
+package BasicPrinciples.OCP;
+
+public class OCP1 {
+    public static void main(String[] args) {
+        /*
+        Open–Closed Principle (OCP)
+        Yazılım yapıları (Sınıflar , fonksiyonlar vs. ) genişletmeye açık ama değişime kapalı olmalıdır.
+        OCP'nin amacı tekrar kullanabilirliği ve bakımlanabilirliği arttırmak.
+        */
+    }
+}
