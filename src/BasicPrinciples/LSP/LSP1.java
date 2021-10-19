1678740285ff646c7e063b1e6876c5d6735e607e
Mehmet Berkan <mehmetberkangorel@gmail.com>
2021-10-19 23:43:45 +0300

    Add LSP README

---
+++ src/BasicPrinciples/LSP/LSP1.java
@@ -0,0 +1,13 @@
+package BasicPrinciples.LSP;
+
+public class LSP1 {
+    public static void main(String[] args) {
+        /*
+        Liskov Substitution Principle (LSP)
+        Taban sınıfa işaretci ya da referans kullanan fonksiyonlar ,
+        türetilmiş sınıfların nesnelerinide bilmeden kullanabilmelidirler.
+        LSP'nin amacı alt sınıflardan oluşan nesnelerin üst sınıfın nesneleri ile yer
+        değiştirdikleri zaman, aynı davranışı sergilemesini beklemektir.
+        */
+    }
+}
