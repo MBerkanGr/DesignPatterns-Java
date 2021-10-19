9ce5b91e63713466272c2add2c1f3f24a279b9ff
Mehmet Berkan <mehmetberkangorel@gmail.com>
2021-10-19 18:16:38 +0300

    Add OCP example

---
+++ src/BasicPrinciples/OCP/Employee_1.java
@@ -0,0 +1,31 @@
+package BasicPrinciples.OCP;
+
+public class Employee_1 {
+    enum EmployeeType {
+        FOREMAN,
+        ENGINEER,
+        CHEF,
+        ARCHITECT
+    }
+
+    public static void PaySalary(EmployeeType type){
+        switch (type){
+            case CHEF:
+                System.out.println("Chef Salary = 3500");
+                break;
+            case FOREMAN:
+                System.out.println("Foreman Salary = 2500");
+                break;
+            case ENGINEER:
+                System.out.println("Engineer Salary = 5500");
+                break;
+            case ARCHITECT:
+                System.out.println("Architect Salary = 5500");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        PaySalary(EmployeeType.ENGINEER);
+    }
+}
