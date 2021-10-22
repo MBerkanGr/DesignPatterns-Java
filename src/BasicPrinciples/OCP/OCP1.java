540b6ade77311b660410edde4aa4e78fc581ca4a
Mehmet Berkan <mehmetberkangorel@gmail.com>
2021-10-22 20:32:12 +0300

    Update OCP README

--- src/BasicPrinciples/OCP/OCP1.java
+++ src/BasicPrinciples/OCP/OCP1.java
@@ -7,5 +7,13 @@ public class OCP1 {
         Yazılım yapıları (Sınıflar , fonksiyonlar vs. ) genişletmeye açık ama değişime kapalı olmalıdır.
         OCP'nin amacı tekrar kullanabilirliği ve bakımlanabilirliği arttırmak.
         */
+
+        /*
+        Employee Örneği
+        Employee_1 sınıfına yeni bir çalışan eklenirse sistem kırılacak ve yeni case eklenmesi gerekecek.
+        Employee_2 ' de ise EmployeeInterface oluşturalarak her eklenen çalışana implement ettiğimiz zaman , sistemi
+        kırmadan direkt eklenebilecek.
+        Böylece kodda değişiklik olmadan yeni çalışan tipleri eklenebilir duruma gelecek.
+        */
     }
 }
