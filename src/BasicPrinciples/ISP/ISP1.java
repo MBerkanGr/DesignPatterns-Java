8baee0d8dc60ddf2e80e041068d1345b0b784f2a
Mehmet Berkan <mehmetberkangorel@gmail.com>
2021-10-25 01:44:26 +0300

    Add ISP README

---
+++ src/BasicPrinciples/ISP/ISP1.java
@@ -0,0 +1,17 @@
+package BasicPrinciples.ISP;
+
+public class ISP1 {
+    public static void main(String[] args) {
+        /*
+        Interface Segregation Principle (ISP)
+        Bütün sorumlulukların tek bir arayüzde bulunmasının yerine özelleştirilip
+        birden fazla arayüze bölünmesini tercih etmemizi ifade eden presiptir.
+        İstemciler kullanmadıkları arayüzlere bağımlı olmaya zorlanmamalıdır.
+        ISP, şişman (fat) ya da kirli (polluted) arayüzlerinden kurtulmak, daha ince,
+        odağı yüksek arayüzlere sahip olmak gerektiğini ifade eder.
+        Bir arayüzün farklı istemcilere hizmet veren metotlardan oluşan hizmet grupları
+        varsa özel ince arayüzlere bölünmelidir.
+        İstemciler sadece ihtiyacı olan metotları görmeliler.
+        */
+    }
+}
