1b96469c6a13b1df9ba54ef8eee60759f4332913
Mehmet Berkan <mehmetberkangorel@gmail.com>
2021-10-26 23:53:13 +0300

    Add DIP README

---
+++ src/BasicPrinciples/DIP/DIP1.java
@@ -0,0 +1,15 @@
+package BasicPrinciples.DIP;
+
+public class DIP1 {
+    public static void main(String[] args) {
+        /*
+        Dependency Inversion Principle (DIP)
+        Yüksek seviyeli modüller aşağı seviye modüllere bağımlı olmamalıdır.
+        İkisi de soyutlamalara bağımlı olmalıdır.
+        Soyutlamalar detaylara bağımlı olmamalı, detaylar soyutlamalara
+        bağımlı olmalıdır.
+        Sınıflar arası bağımlılıklar olabildiğince az olmalıdır özellikle
+        üst seviye sınıflar alt seviye sınıflara bağımlı olmamalıdır.
+        */
+    }
+}
