19cc8a045b1a56dca6042d715afc11bfdfad0f0f
Mehmet Berkan <80527393+MBerkanGr@users.noreply.github.com>
2023-06-19 15:11:51 +0300

    Update Singleton

--- src/Creational/Singleton/Singleton.java
+++ src/Creational/Singleton/Singleton.java
@@ -1,18 +1,25 @@
 package Creational.Singleton;
 
 public class Singleton {
-
     private static Singleton instance;
     private static int n = 0;
+    private static int m = 0;
 
-    private Singleton(){}
+    private Singleton(){
+        System.out.println(n);
+    }
 
     public static Singleton getInstance(){
-
-        if(instance == null){
-            n++;
-            instance = new Singleton();
+        m++;
+        if(instance == null) {
+            synchronized (Singleton.class){
+                if(instance == null){
+                    n++;
+                    instance = new Singleton();
+                }
+            }
         }
+        System.out.println(m);
         return instance;
     }
 
